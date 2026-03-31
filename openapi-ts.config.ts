f2fb6d4532cb2a6cf3cf097de4f9627018fe6b85
Isutomu <isutomu@gmail.com>
2026-03-31 14:05:55 -0300

    :bug: fix: Fix the initial implementation
    Some aspects were reworked to be more homogeneous, security is now by
    case basis, and a basic route (verify-session) was added.
    Updates will proceed as normal from here.

--- openapi-ts.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "@hey-api/openapi-ts";
-
-export default defineConfig({
-  input: "./openapi/openapi.yaml", // sign up at app.heyapi.dev
-  output: "src/client",
-  plugins: ["@tanstack/react-query"],
-});
